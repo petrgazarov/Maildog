9d9773acab312e654339cdf1c374f5b9dbaa5095
Petr Gazarov <petrgazarov@gmail.com>
2015-09-15 16:55:52 -0400

    add validations to EmailAddressee model

--- app/models/email_addressee.rb
+++ app/models/email_addressee.rb
@@ -1,2 +1,10 @@
 class EmailAddressee < ActiveRecord::Base
+  validates :email, :addressee, :type, presence: true
+
+  belongs_to :email,
+    foreign_key: :email_id
+
+  belongs_to :addressee,
+    class_name: "Contact",
+    foreign_key: :addressee_id
 end
