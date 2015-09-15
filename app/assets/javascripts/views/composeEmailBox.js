da2cec552edb36bfd951f94ead9e05bed978c1cd
Petr Gazarov <petrgazarov@gmail.com>
2015-09-15 14:40:47 -0400

    create composeEmailBox.jst.ejs form

--- app/assets/javascripts/views/composeEmailBox.js
+++ app/assets/javascripts/views/composeEmailBox.js
@@ -1,1 +1,9 @@
+Maildog.Views.ComposeEmailBox = Backbone.CompositeView.extend({
+  template: JST['composeEmailBox'],
+  tagName: 'form',
 
+  render: function() {
+    this.$el.html(this.template());
+    return this;
+  }
+});
