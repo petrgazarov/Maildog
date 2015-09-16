2628f5b2ca5753b621853269a3f330264b7a8260
Petr Gazarov <petrgazarov@gmail.com>
2015-09-16 10:05:00 -0400

    refactor MailIndex#render

--- app/assets/javascripts/views/mailIndex.js
+++ app/assets/javascripts/views/mailIndex.js
@@ -2,28 +2,29 @@ Maildog.Views.MailIndex = Backbone.CompositeView.extend({
   template: JST['mailIndex'],
 
   initialize: function() {
-
   },
 
   render: function() {
     this.$el.html(this.template());
-
-    [ Maildog.Views.SearchBar,
-      Maildog.Views.CurrentUser,
-      Maildog.Views.PagesMenu,
-      Maildog.Views.EmailOptions,
-      Maildog.Views.EmailNavigation,
-      Maildog.Views.EmailFolders ].forEach(function(view) {
-        this.initializeView(view);
-      }.bind(this))
-
+    this._initializeSubviews();
     return this;
   },
 
-  initializeView: function(view) {
-    // debugger
-    var initializedView = new view();
-    this.$('#mail-index-content').append(initializedView.render().$el)
-    // this.addSubview(this.$el, initializedView);
+  _initializeSubviews: function() {
+    [ Maildog.Views.SearchBar,
+      Maildog.Views.CurrentUser ].forEach(function(view) {
+        this.addSubview(this.$('.mail-header'), new view());
+      }.bind(this));
+
+    [ Maildog.Views.PagesMenu,
+      Maildog.Views.EmailOptions,
+      Maildog.Views.EmailNavigation ].forEach(function(view) {
+        this.addSubview(this.$('.mail-nav'), new view());
+      }.bind(this))
+
+    this.addSubview(
+      this.$('#mail-index-content'),
+      new Maildog.Views.EmailFolders()
+    );
   }
 });
