9d066a36cbd48a5833f962d06302906fb30e0ccb
Petr Gazarov <petrgazarov@gmail.com>
2015-09-16 20:49:58 -0400

    refactor EmailList View to add subviews

---
+++ app/assets/javascripts/views/emailList.js
@@ -0,0 +1,22 @@
+Maildog.Views.EmailList = Backbone.CompositeView.extend({
+  template: JST['emailList'],
+
+  initialize: function() {
+    this.collection.fetch();
+    this.listenTo(this.collection, 'sync', this.render);
+    this.listenTo(this.collection, 'add', this.addSubviewForEmail);
+
+    this.collection.forEach(this.addSubviewForEmail);
+  },
+
+  render: function() {
+    this.$el.html(this.template())
+    this.attachSubviews();
+    return this;
+  },
+
+  addSubviewForEmail: function(email) {
+    var subview = new Maildog.Views.EmailListItem({ model: email });
+    this.addSubview(".email-list", subview);
+  }
+});
