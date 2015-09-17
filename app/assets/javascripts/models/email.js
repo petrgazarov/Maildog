6471d3886016d322b4aab1c5e1176d00a7d3814a
Petr Gazarov <petrgazarov@gmail.com>
2015-09-16 21:44:55 -0400

    add #parse and #sender to Email Model

--- app/assets/javascripts/models/email.js
+++ app/assets/javascripts/models/email.js
@@ -1,3 +1,16 @@
 Maildog.Models.Email = Backbone.Model.extend({
-  urlRoot: "api/emails"
+  urlRoot: "api/emails",
+
+  parse: function(payload) {
+    if (payload.sender) {
+      this.sender().set(payload.sender, { parse: true })
+      delete payload.sender
+    }
+    return payload
+  },
+
+  sender: function() {
+    this._sender || (this._sender = new Maildog.Models.Contact())
+    return this._sender
+  }
 });
