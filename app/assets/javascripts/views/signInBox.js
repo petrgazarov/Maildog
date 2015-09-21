9f59912874caf34253003c461dcdef0358bde2bc
Petr Gazarov <petrgazarov@gmail.com>
2015-09-20 20:18:39 -0400

    move canvas inside the sign in form

--- app/assets/javascripts/views/signInBox.js
+++ app/assets/javascripts/views/signInBox.js
@@ -1,15 +1,10 @@
 Maildog.Views.SignInBox = Backbone.View.extend({
   tagName: "form",
 
-  events: {
-    "submit": "transitionOut"
-  },
-
-  templateUser: JST['signInUser'],
-  templateAuth: JST['signInPassword'],
+  template: JST['signInBox'],
 
   render: function() {
-    var content = this.templateUser({ user: Maildog.currentUser });
+    var content = this.template({ user: Maildog.currentUser });
     this.$el.html(content);
     return this;
   },
@@ -25,9 +20,5 @@ Maildog.Views.SignInBox = Backbone.View.extend({
 
   tryAgain: function() {
     alert("Sorry, Maildog doesn't recognize that email.");
-  },
-
-  transitionOut: function(e) {
-    e.preventDefault();
   }
 });
