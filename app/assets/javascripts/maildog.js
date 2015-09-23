071be0cb41fb3827998d96ecc382433eab3aef0e
Petr Gazarov <petrgazarov@gmail.com>
2015-09-23 11:18:57 -0400

    move initialization logic into Maildog#initialize

--- app/assets/javascripts/maildog.js
+++ app/assets/javascripts/maildog.js
@@ -4,10 +4,26 @@ window.Maildog = {
   Views: {},
   Routers: {},
   initialize: function() {
-  
+    Maildog.currentUser = (
+      Maildog.currentUser || new Maildog.Models.CurrentUser()
+    );
+    Maildog.inboxEmails = new Maildog.Collections.Emails(
+      [], { urlAction: "inbox" });
+    Maildog.currentUser.fetch();
+    Backbone.pubSub = _.extend({}, Backbone.Events);
+
+    Maildog.flashMessages = new Maildog.Views.FlashMessageList();
+    $('.flash-container').html(Maildog.flashMessages.render().$el);
+
+    Maildog.router = new Maildog.Routers.Router({
+      $rootEl: $('.main-container')
+    });
+
+    var mailNavView = new Maildog.Views.MailNav();
+    $("#mail-nav").html(mailNavView.render().$el);
+    var mailSidebarView = new Maildog.Views.MailSidebar();
+    $("#mail-sidebar").html(mailSidebarView.render().$el);
+
+    Backbone.history.start();
   }
 };
-
-$(function() {
-  Maildog.initialize();
-})
