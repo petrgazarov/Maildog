ccd8fa7eedc13632dbc726c2979a03da6a645c02
Petr Gazarov <petrgazarov@gmail.com>
2015-09-27 13:54:37 -0400

    create SearchResults collection

---
+++ app/assets/javascripts/collections/search_results.js
@@ -0,0 +1,18 @@
+Maildog.Collections.SearchResults = Backbone.Collection.extend({
+	url: "/api/search",
+
+	parse: function (resp) {
+		if (resp.total_count) {
+			this.total_count = resp.total_count;
+		}
+
+		return resp.results;
+	},
+
+	model: function (attrs) {
+		var type = attrs._type;
+		delete attrs._type;
+
+		return new Maildog.Models[type](attrs);
+	}
+});
