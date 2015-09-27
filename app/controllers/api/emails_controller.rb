0d19b58a60056251d113540ffc604a32a7ad09b3
Petr Gazarov <petrgazarov@gmail.com>
2015-09-27 19:46:34 -0400

    add Email #changed_star_or_check and refactor EmailsController

--- app/controllers/api/emails_controller.rb
+++ app/controllers/api/emails_controller.rb
@@ -11,7 +11,10 @@ class Api::EmailsController < ApplicationController
   def update
     @email = Email.find(params[:id])
     @email.draft = params[:email][:draft]
-    if @email.draft
+    if @email.draft ||
+        @email.changed_star_or_check(
+          params[:email][:starred], params[:email][:checked]
+        )
       update_email(@email)
     else
       persist_and_send_email(:update, @email)
--- app/models/email.rb
+++ app/models/email.rb
@@ -46,6 +46,10 @@ class Email < ActiveRecord::Base
     self.time ||= Time.now.strftime("%I:%M:%S %z")
   end
 
+  def changed_star_or_check(new_starred, new_checked)
+    !self.starred == new_starred && self.checked == new_checked
+  end
+
   def sender_first_name
     sender.first_name
   end
