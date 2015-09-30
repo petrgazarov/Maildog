a82de1432bf93b824d4e9e210f71b40d4873a37f
Petr Gazarov <petrgazarov@gmail.com>
2015-09-30 15:35:24 -0400

    write initialize and addSubviewForFolder methods on CustomFolders View

--- app/assets/javascripts/views/customFolders.js
+++ app/assets/javascripts/views/customFolders.js
@@ -5,15 +5,26 @@ Maildog.Views.CustomFolders = Backbone.CompositeView.extend({
     "click #create-new-folder-button": "newFolder"
   },
 
+  initialize: function() {
+    this.collection = new Maildog.Collections.Folders();
+    this.collection.fetch({ reset: true });
+    this.listenTo(this.collection, "reset", this.render);
+    this.listenTo(
+      this.collection, "add", this.addSubviewForFolder.bind(this, true)
+    );
+  },
+
   render: function() {
     this.$el.html(this.template());
+    this.collection.forEach(this.addSubviewForFolder.bind(this, false));
     return this;
   },
 
-  newFolder: function() {
+  addSubviewForFolder: function(folder, prepend) {
     var subview = new Maildog.Views.FolderListItem({
-      collection: 
+      collection: this.collection,
+      model: folder
     });
-    this.addSubview(".custom-folders-list", subview, true);
+    this.addSubview(".custom-folders-list", subview, prepend);
   }
 });
