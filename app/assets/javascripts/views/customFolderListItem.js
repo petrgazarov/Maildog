8d784f7e501cc79c74bf1ba16ac3e27b2a7904cf
Petr Gazarov <petrgazarov@gmail.com>
2015-09-30 16:39:58 -0400

    write CustomFolderListItem #saveOrUpdateFolder

--- app/assets/javascripts/views/customFolderListItem.js
+++ app/assets/javascripts/views/customFolderListItem.js
@@ -13,6 +13,23 @@ Maildog.Views.CustomFolderListItem = Backbone.View.extend({
   },
 
   showTextBox: function() {
-    this.$('input').removeClass('invisible');
+    this.$('input').removeClass('invisible').focus();
+
+    window.setTimeout(function() {
+      $('html').click(function() {
+        this.saveOrUpdateFolder({ text: this.$('input').val });
+      }.bind(this))
+    }.bind(this), 0);
+  },
+
+  saveOrUpdateFolder: function(options) {
+    $('html').off('click');
+
+    this.model.save({ name: options.text }, {
+      success: function() {
+        this.render();
+      }.bind(this)
+    });
+    this.$('input').addClass('invisible');
   }
 });
