8fc9aa19cb02f564a0de55f9fdb570d1dc52edf4
Petr Gazarov <petrgazarov@gmail.com>
2015-09-30 12:45:13 -0400

    create customFolders template

--- app/assets/javascripts/views/customFolders.js
+++ app/assets/javascripts/views/customFolders.js
@@ -2,6 +2,7 @@ Maildog.Views.CustomFolders = Backbone.CompositeView.extend({
   template: JST['customFolders'],
 
   render: function() {
-    
+    this.$el.html(this.template());
+    return this;
   }
 })
