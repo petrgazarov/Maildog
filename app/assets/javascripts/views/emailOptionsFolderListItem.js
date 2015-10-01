c089caeb832bcce5267da1f3fcaf5a9bb18e4e06
Petr Gazarov <petrgazarov@gmail.com>
2015-10-01 15:00:29 -0400

    add loadFolder to EmailOptionsFolderListItem

--- app/assets/javascripts/views/emailOptionsFolderListItem.js
+++ app/assets/javascripts/views/emailOptionsFolderListItem.js
@@ -2,9 +2,17 @@ Maildog.Views.EmailOptionsFolderListItem = Backbone.View.extend({
   template: JST['emailOptionsFolderListItem'],
   tagName: "li",
 
+  events: {
+    "click": "loadFolder"
+  },
+
   render: function() {
     var content = this.template({ folder: this.model });
     this.$el.html(content);
     return this;
+  },
+
+  loadFolder: function() {
+    Backbone.history.navigate("folders/" + this.model.id, { trigger: true });
   }
 });
