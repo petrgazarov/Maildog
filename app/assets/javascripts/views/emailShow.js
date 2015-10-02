608d04834c6a7dc2d847a1df6705b1fdd7d014b4
Petr Gazarov <petrgazarov@gmail.com>
2015-10-01 22:46:20 -0400

    add star functionality to emailshow

--- app/assets/javascripts/views/emailShow.js
+++ app/assets/javascripts/views/emailShow.js
@@ -3,9 +3,30 @@ Maildog.Views.EmailShow = Backbone.CompositeView.extend({
   tagName: "ul",
   className: "email-show-item group",
 
+  events: {
+    "click .star": "starClick"
+  },
+
   render: function() {
     var content = this.template({ email: this.model });
     this.$el.html(content);
     return this;
-  }
+  },
+
+  starClick: function(e) {
+    e.preventDefault();
+
+    $(e.currentTarget).toggleClass('star-on');
+    if (this.model.get('starred')) {
+      this.model.set('starred', false)
+    } else {
+      this.model.set('starred', true)
+    }
+
+    this.model.save({}, {
+      error: function() {
+        alert('error')
+      }
+    })
+  },
 });
