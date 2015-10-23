1f4ee0c9dfdb499facd8440b225547f7822e54fb
Petr Gazarov <petrgazarov@gmail.com>
2015-10-23 17:28:50 -0400

    write Contact associations and validations specs

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 
 RSpec.describe Contact do
-  it "must have an email" do
-    expect(build(:contact, email: nil)).not_to be_valid
-  end
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:owner) }
+  it { should belong_to(:owner) }
+  it { should have_many(:labels) }
+  it { should have_many(:threads) }
+  it { should have_many(:received_emails) }
+  it { should have_many(:written_emails) }
+  it { should have_many(:email_addressees) }
 end
