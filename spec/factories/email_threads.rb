565c1c5d75b2b0fc2b1ab46ae001bc70b98da803
Petr Gazarov <petrgazarov@gmail.com>
2015-10-25 15:32:08 -0400

    write specs for EmailThread #trash_count and #non_trash_count

--- spec/factories/email_threads.rb
+++ spec/factories/email_threads.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
   factory :email_thread do
     association :owner, factory: :contact
-
     subject Faker::Lorem.word
+
+    factory :email_thread_with_emails do
+      after(:build) do |email_thread|
+        3.times { create(:email, thread: email_thread) }
+        create(:email, thread: email_thread, trash: true)
+      end
+    end
   end
 end
--- spec/models/email_thread_spec.rb
+++ spec/models/email_thread_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe EmailThread do
   it "is valid with owner" do
     expect(build(:email_thread)).to be_valid
   end
+
+  describe "#trash_count" do
+    it "returns the correct count of trash emails that belong to the thread" do
+      expect(build(:email_thread_with_emails).trash_count).to eq(1)
+    end
+  end
+
+  describe "#non_trash_count" do
+    it "returns the correct count of non trash emails that belong to the thread" do
+      expect(build(:email_thread_with_emails).non_trash_count).to eq(3)
+    end
+  end
 end
