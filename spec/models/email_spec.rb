aa5eceb97ca455fdf3a7a3e3a3dcf65e3f4f06c4
Petr Gazarov <petrgazarov@gmail.com>
2015-10-25 01:22:33 -0400

    write specs for pg_search helper methods on Email

--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -8,6 +8,10 @@ RSpec.describe Email do
   it { should belong_to(:original_email) }
   it { should belong_to(:thread) }
 
+  it "is valid without any set attributes" do
+    expect(build(:email)).to be_valid
+  end
+
   describe "#changed_star_or_trash?" do
     before(:all) do
       @email = build(:email)
@@ -23,6 +27,35 @@ RSpec.describe Email do
 
     it "returns false if given same star and trash values" do
       expect(@email.changed_star_or_trash?(false, false)).to be false
+    end
+  end
+
+  describe "pg_search helper methods" do
+    before(:each) do
+      @email = build(:email)
+    end
+
+    describe "#sender_first_name" do
+      it "returns sender's first name" do
+        expect(@email.sender_first_name).to eq(@email.sender.first_name)
+      end
+    end
+
+    describe "#sender_last_name" do
+      it "returns sender's last name" do
+        expect(@email.sender_last_name).to eq(@email.sender.last_name)
+      end
+    end
+
+    describe "#not_trash?" do
+      it "returns true when email is not trash" do
+        expect(@email.not_trash?).to be true
+      end
+
+      it "returns false when email is trash" do
+        @email.trash = true
+        expect(@email.not_trash?).to be false
+      end
     end
   end
 end
--- spec/models/email_thread_spec.rb
+++ spec/models/email_thread_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe EmailThread do
   it { should belong_to(:owner) }
   it { should have_many(:thread_labels) }
   it { should have_many(:labels) }
+  it { validate_presence_of(:owner) }
+
+  it "is valid with owner" do
+    expect(build(:email_thread)).to be_valid
+  end
 end
